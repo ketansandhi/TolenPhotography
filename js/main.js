d9a7863831deca333db5c8ccb634d90938e47231
ketan <ketan.sandhi@gmail.com>
2020-07-16 17:52:33 -0600

    updated design

---
+++ js/main.js
@@ -0,0 +1,7 @@
+
+/** code by webdevtrick ( https://webdevtrick.com ) **/
+[...document.querySelectorAll('.single-column')].map(column => {
+	column.style.setProperty('--animation', 'slide');
+	column.style.setProperty('height', '200%');
+	column.innerHTML = column.innerHTML + column.innerHTML;
+});
